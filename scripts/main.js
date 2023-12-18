bbf129f8bf047dbe838a3ec244ee3d50d58dcbac
Alexander Kuiper <kuiper9000@gmail.com>
2023-12-18 14:11:11 +0100

    added a class to test

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,41 @@
+class Linkdiv {
+  constructor(link, parent) {
+    this.link = link;
+    this.parent = parent;
+  }
+
+  add() {
+    //create new div
+    const newLinkDiv = document.createElement("div");
+    //auto label maker
+    const finalDot = this.link.lastIndexOf(".");
+    let sliceAmount;
+    if (/www\./i.test(this.link)) {
+      sliceAmount = 12;
+    } else {
+      sliceAmount = 8;
+    }
+    //attributes
+    newLinkDiv.id = "linkDiv-" + this.link.slice(sliceAmount, finalDot);
+    newLinkDiv.className = "linkDiv";
+    //format
+    newLinkDiv.innerHTML = `<img src="https://s2.googleusercontent.com/s2/favicons?domain=${
+      this.link
+    }"/> <a href=${this.link} target= _blank> ${this.link.slice(
+      sliceAmount,
+      finalDot
+    )}</a><button class=deleteLink> x </button>`;
+    //eventListners
+    const deleteBtn = newLinkDiv.querySelector("button");
+    deleteBtn.addEventListener("click", (e) => {
+      removeLink(e.target.parentNode.id);
+    });
+    this.parent.appendChild(newLinkDiv);
+  }
+
+  remove() {}
+}
+
 function renderTime() {
   const time = document.querySelector("header");
   //get current date
@@ -42,16 +80,21 @@ function dashboardName() {
 }
 
 function addLinks() {
-  const tempDivDelete = document.getElementById("links");
+  //get and create elements
+  const parentDiv = document.getElementById("links");
   const linkBtn = document.getElementById("linkBtn");
   const linksInput = document.createElement("input");
+  //Attributs for textinput
   linksInput.type = "text";
   linksInput.placeholder = "Write link to add";
   linksInput.id = "linkText";
   linksInput.className = "plusBtn";
+  //event listener when the user press enter take all info in.
   linksInput.addEventListener("keydown", (buttonPress) => {
     if (buttonPress.key == "Enter") {
+      //prevent the next line
       buttonPress.preventDefault();
+      //check if local storage is empty
       if (linksInput.value != "") {
         let linkKey = localStorage.getItem("links").split(",");
         if (linkKey[0] == "") {
@@ -61,8 +104,9 @@ function addLinks() {
         }
         localStorage.setItem("links", linkKey);
       }
+      //create new div
+      new Linkdiv(linksInput.value, parentDiv).add();
 
-      renderLinks();
       linksInput.replaceWith(linkBtn);
     }
   });
@@ -74,22 +118,25 @@ function renderLinks() {
   const container = document.getElementById("links");
   if (allLinks[0] != "") {
     allLinks.forEach((link) => {
-      const newLinkDiv = document.createElement("div");
-      const finalDot = link.lastIndexOf(".");
-      let sliceAmount;
-      if (/www\./i.test(link)) {
-        sliceAmount = 12;
-      } else {
-        sliceAmount = 8;
-      }
-      newLinkDiv.id = "linkDiv-" + link.slice(sliceAmount, finalDot);
-      newLinkDiv.className = "linkDiv";
-      newLinkDiv.innerHTML = `<img src="https://s2.googleusercontent.com/s2/favicons?domain=${link}"/> <a href=${link} target= _blank> ${link.slice(
-        sliceAmount,
-        finalDot
-      )}</a><button class=deleteLink> x </button>`;
+      // const newLinkDiv = document.createElement("div");
+      // const finalDot = link.lastIndexOf(".");
+      // let sliceAmount;
+      // if (/www\./i.test(link)) {
+      //   sliceAmount = 12;
+      // } else {
+      //   sliceAmount = 8;
+      // }
+      // newLinkDiv.id = "linkDiv-" + link.slice(sliceAmount, finalDot);
+      // newLinkDiv.className = "linkDiv";
+      // newLinkDiv.innerHTML = `<img src="https://s2.googleusercontent.com/s2/favicons?domain=${link}"/> <a href=${link} target= _blank> ${link.slice(
+      //   sliceAmount,
+      //   finalDot
+      // )}</a><button class=deleteLink> x </button>`;
+      new Linkdiv(link, container).add();
 
-      container.appendChild(newLinkDiv);
+      // const newLinkDiv = new Linkdiv(link, container);
+      // newLinkDiv.add();
+      // container.appendChild(newLinkDiv.add());
     });
   }
 }
@@ -101,27 +148,17 @@ function initializeAll() {
   renderTime();
   setInterval(renderTime, 1000);
   renderLinks();
-  //adding eventlistners
-  const linksCard = document.getElementById("links");
-  const linkBtn = document.getElementById("linkBtn");
-  linksCard.addEventListener("click", (e) => {
-    if (e.target.className == "deleteLink") {
-      removeLink(e.target.parentNode.id);
-    }
-  });
-
   linkBtn.addEventListener("click", addLinks);
 }
 
 function removeLink(id) {
   const deleteDiv = document.getElementById(id);
-  const deleteLink = deleteDiv.querySelector("a");
+  const deleteLink = deleteDiv.querySelector("a").getAttribute("href");
   const allLinks = localStorage.getItem("links").split(",");
   localStorage.setItem(
     "links",
-    allLinks.filter((links) => links != deleteLink.getAttribute("href"))
+    allLinks.filter((links) => links != deleteLink)
   );
 
   deleteDiv.remove();
-  renderLinks();
 }
